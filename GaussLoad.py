cd63eaa1e6ce8a73c8e9f9faadc18ff4f97c0e7a
SB-27182 <sjbrower62831@gmail.com>
2022-02-01 03:14:42 -0500

    commit archive

---
+++ GaussLoad.py
@@ -0,0 +1,146 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+
+
+#We're Making This Thing GodTier Obviously.
+k=3
+class Gauss_nnMLE(nn.Module):
+    def __init__(self):
+        #For Sig and Pi, we have a scalar for each mixture kernel.
+        #For Mu, we have a t size vector; a mean for every dimension of y, and one of those said vectors for each kernel.
+        super(Gauss_nnMLE, self).__init__()
+                           #from  #into
+        self.lH1 = nn.Linear(1, 50, bias=True)
+        self.lH2 = nn.Linear(50, 100, bias=True)
+        self.lH3 = nn.Linear(100,50, bias=True)
+        self.lPi = nn.Linear(50, k, bias=True)
+        self.lMu = nn.Linear(50, k*1, bias=True)
+        self.lSigSq = nn.Linear(50, k, bias=True) #Not using full covariance. lSigSq only for every kernel.
+
+
+
+    def forward(self, x):
+       # print("")
+      #  print("This is the weights", list(self.lH1.named_parameters()))
+        x = torch.tanh(self.lH1(x))
+        x = torch.tanh(self.lH2(x))
+        z = torch.tanh(self.lH3(x))
+
+        pi = F.softmax(self.lPi(z), dim=1)
+        sigSq = torch.exp(self.lSigSq(z))
+        mu = self.lMu(z)
+
+        return (pi, mu, sigSq)
+
+def mixCoefs_3Pi(model, ll, ul):
+ #Given a model, and input values (x values) ll, ul, generates (ul-ll)*10 mixing coefficient values
+ #This has to load (ul-ll)*10 models ... it takes a bit to run..
+ #returns a tuple of FOUR lists: x, pi0, pi1, pi2
+    """MODEL MUST BE IN EVAL MODE AND MODEL MUST BE MOVED TO CUDA GPU BEFORE CALLING THIS"""
+    xList = []
+    pi0List = []
+    pi1List = []
+    pi2List = []
+    i = ll
+    while(i < ul):
+        tens_i = torch.tensor([[i]], device="cuda:0")
+        distr = model(tens_i)
+        pi = distr[0][0]
+
+        pi0List.append(pi[0].item())
+        pi1List.append(pi[1].item())
+        pi2List.append(pi[2].item())
+        xList.append(i)
+
+        i = i + 0.1
+
+    return (xList, pi0List, pi1List, pi2List)
+
+
+
+
+class Distrx():
+    def __init__(self, pi:torch.Tensor, mu:torch.Tensor, sig2:torch.Tensor, x:float):
+        self.m = torch.distributions.multinomial.Multinomial(total_count=1, probs=pi)
+        self.x = x
+
+        g0 = (torch.tensor([1.0, 0.0, 0.0], device="cuda:0"), torch.distributions.normal.Normal(mu[0], sig2[0]))
+        g1 = (torch.tensor([0.0, 1.0, 0.0], device="cuda:0"), torch.distributions.normal.Normal(mu[1], sig2[1]))
+        g2 = (torch.tensor([0.0, 0.0, 1.0], device="cuda:0"), torch.distributions.normal.Normal(mu[2], sig2[2]))
+        #g3 = (torch.tensor([0.0, 0.0, 0.0, 1.0], device="cuda:0"), torch.distributions.normal.Normal(mu[1], sig2[2]))
+        self.kernels = [g0, g1, g2]
+
+    def outPuts(self, count):
+        "WARNING; RETURNS A LIST OF FLOATS!"""
+        outputs = []
+        i = 0
+        while(i < count):
+            m = self.m.sample()
+
+            for j in self.kernels:
+                if torch.equal(j[0], m):
+                    target = j[1].sample()
+                    target = target.item()
+
+                    outputs.append(target)
+
+            i=i+1
+        return outputs
+
+
+
+def main():
+    model = Gauss_nnMLE()
+    paramDict = torch.load("./data/mixModel1.pt")
+    model.load_state_dict(paramDict)
+
+    model.to(device="cuda:0")
+    model.eval()
+
+
+    with torch.no_grad():
+        x = -3.0
+        testOutputs = []
+        testInputs = []
+        while(x < 3.0):
+            tensX = torch.tensor([[x]], device="cuda:0")
+            testM = model(tensX)
+            testDist = Distrx(pi=testM[0][0], mu=testM[1][0], sig2=testM[2][0], x=x)
+            testOutputs.append((testDist.outPuts(20)))
+            testInputs.append([x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x])
+            x = x + 0.025
+    fig, ax = plt.subplots(1, 1, figsize=(12, 12))
+    ax.set_xlim([-1.5, 1.5])
+    ax.set_ylim([-1.5, 1.5])
+    ax.plot(testInputs, testOutputs, 'ro')
+    plt.show()
+
+
+    with torch.no_grad():
+        tuple = mixCoefs_3Pi(model, -3.0, 3.0)
+        xVals = tuple[0]
+    fig, ax = plt.subplots(1, 1, figsize=(12, 12))
+    ax.plot(xVals, tuple[1], label="pi_0")
+    ax.plot(xVals, tuple[2], label="pi_1")
+    ax.plot(xVals, tuple[3], label="pi_2")
+
+    plt.show()
+#From an x value, through the marginal PMF for pi, sample from the 3 kernels. Make 50 samples each. Output array.
+
+
+   # i = 0
+  #  while(i < 600):
+  #      print(tuple[1][i])
+  #      i = i + 1
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ GaussMM_diag_logAndLinnear.py
@@ -0,0 +1,291 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+
+
+#So we will make the vectors, forward these vectors
+batch_size = 251
+class testDataSet(Dataset):
+    def __init__(self):
+        #initializing data rep [data loading]
+        xy = np.loadtxt("./data/test_2_values.txt", delimiter=",", dtype=np.float32, skiprows=1)
+        x = xy[:, [0]]#We want all the rows, and first column
+        y = xy[:, 1:3]#We want all the rows, and second column       # TWO BRACKETS MEANS IT WILL BE A (276 x 1) 2-dim TENSOR
+        self.x = torch.from_numpy(x)                                  # xy[:, 0] MEANS IT WILL BE A (276) 1-dim TENSOR
+        self.y = torch.from_numpy(y)
+        self.n__samples = xy.shape[0]
+
+    def __getitem__(self, index):
+        #dataset[i] for indexing
+        return (self.x[index], self.y[index])
+
+
+    def __len__(self):
+        return self.n__samples
+
+
+
+k = 3
+d = 2
+#We're Making This Thing GodTier Obviously.
+class Gauss_nnMLE(nn.Module):
+    def __init__(self):
+        #For Sig and Pi, we have a scalar for each mixture kernel.
+        #For Mu, we have a t size vector; a mean for every dimension of y, and one of those said vectors for each kernel.
+        super(Gauss_nnMLE, self).__init__()
+                           #from  #into
+        self.lH1 = nn.Linear(1, 50, bias=True)
+        self.lH2 = nn.Linear(50, 100, bias=True)
+        self.lH3 = nn.Linear(100,50, bias=True)
+        self.lPi = nn.Linear(50, k, bias=True)
+        self.lMu = nn.Linear(50, k*d, bias=True)
+        self.lVars = nn.Linear(50, k*d, bias=True) #Not using full covariance. lSigSq only for every kernel.
+
+
+
+    def forward(self, x):
+        x = torch.tanh(self.lH1(x))
+        x = torch.tanh(self.lH2(x))
+        z = torch.tanh(self.lH3(x))
+
+        pi = F.softmax(self.lPi(z), dim=1)
+        mu = self.lMu(z)
+        vars = torch.exp(self.lVars(z))
+
+
+        return (pi, mu, vars)
+
+
+def logLoss_MLE(target:torch.Tensor, pi:torch.Tensor, mu:torch.Tensor, vars:torch.Tensor):
+    """Input sizes:
+       target.size() = [batch, dim]
+       pi.size() = [batch, kernel]
+       mu.size() = var.size() = [batch, outputDim*kernel]
+       covar.size() = [batch, dim*kernel*(dim-1)/2]"""
+
+    mu = mu.view(batch_size, k, d)
+    vars = vars.view(batch_size, k, d)
+
+    target = target.view(batch_size, 1, d)
+
+    i = 0
+    losses = torch.zeros(size=(1, batch_size), device='cuda:0')
+    while(i < k):  # 3 values in PMF of K
+        k_mu = mu[:, [i], :]  #<------------------- THIS [:, [i]] NOTATION MEANS WE WANT THE TRANSPOSE
+        k_var = vars[:, [i], :]
+
+
+        likelihood = gaussian_pdf(target, k_mu, k_var)
+        k_p = pi[:, [i]]
+
+        losses = torch.log(k_p) + likelihood + losses #Sum all k kernels
+        i = i + 1
+
+    loss = torch.mean(-losses)
+    return loss
+
+
+def gaussian_pdf(y, mu, vars):
+    sub = torch.sub(y, mu)
+    sigma = diagMatrixMake(vars)
+    clone = torch.clone(vars)
+    ln_det = torch.sum(torch.log(clone))
+
+    sigmaInv = torch.linalg.inv(sigma)
+    a = torch.bmm(input=sub, mat2=sigmaInv)
+    nobis = torch.bmm(input=a, mat2=torch.transpose(a, dim0=1, dim1=2))
+
+    return -0.5*(ln_det + nobis) -0.5*d*np.log(2*np.pi)
+
+def diagMatrixMake(vars):
+    """Given a d*1 tensor, outputs a d*d tensor with non-diagnol values of 0."""
+    diag = torch.diag_embed(vars, dim1=2, dim2=1)
+    return diag.reshape([batch_size, d, d])
+
+
+
+
+def linnearLoss_MLE(target:torch.Tensor, pi:torch.Tensor, mu:torch.Tensor, vars:torch.Tensor):
+    """Input sizes:
+       target.size() = [batch, dim]
+       pi.size() = [batch, kernel]
+       mu.size() = var.size() = [batch, outputDim*kernel]
+       covar.size() = [batch, dim*kernel*(dim-1)/2]"""
+
+    mu = mu.view(batch_size, k, d)
+    vars = vars.view(batch_size, k, d)
+
+    target = target.view(batch_size, 1, d)
+
+    i = 0
+    losses = torch.zeros(size=(1, batch_size), device='cuda:0')
+    while(i < k):  # 3 values in PMF of K
+        k_mu = mu[:, [i], :]  #<------------------- THIS [:, [i]] NOTATION MEANS WE WANT THE TRANSPOSE
+        k_var = vars[:, [i], :]
+
+
+        likelihood = linr_gaussian_pdf(target, k_mu, k_var)
+        k_p = pi[:, [i]]
+
+        losses = k_p * likelihood + losses
+        i = i + 1
+
+    loss = torch.mean(-losses)
+    return loss
+
+
+def linr_gaussian_pdf(y, mu, vars):
+    sub = torch.sub(y, mu)
+
+    sigma = diagMatrixMake(vars)
+    clone = torch.clone(sigma)
+
+    sigmaInv = torch.linalg.inv(sigma)
+    a = torch.bmm(input=sub, mat2=sigmaInv)
+    nobis = torch.bmm(input=a, mat2=torch.transpose(a, dim0=1, dim1=2))
+    fac1 = torch.exp(-0.5 * nobis)
+
+    fac2 = torch.pow(torch.sqrt(torch.linalg.det(clone)), exponent=-1)
+    fac3 = np.sqrt(np.pi*2) ** -d
+
+    return fac3 * fac2 * fac1
+
+
+
+
+
+def main():
+
+    def piHook(self, grad_input, grad_output):
+        #grad_output = grad_output*10
+       #print("THIS IS GRAD INPUT", grad_input)
+       # print("THIS IS GRAD OUTPUT", grad_output)
+        return grad_output*10
+
+    def forwardHook(self, input, output):#You can just print in the forward itself. Hooks are for dynamic actions.
+        print("==================================================================")
+        print("")
+        print("input", input)
+        print("==================================================================")
+        print("")
+        print("output", output)
+        print("==================================================================")
+        print("")
+
+
+#==========[model]===================
+    model = Gauss_nnMLE()
+    model.to(device="cuda:0")
+
+#==========[dataLoader]==============
+    dataset = testDataSet()
+    dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True) #I THINK BATCH SIZES SHOULD BE A FACTOR OF THE DATA SIZE FOR GRADIENT DESCENT!
+                                                                #  The reason is otherwise you will have the one last batch using less data!
+    #Full batch size = gradient descent                       #   And the issue is that the gradient step still listens to this "less informed" gradient step
+    #Not full batch size = stochastic gradient descent       #    Which is interesting; if the step size for the "less informed" was smaller it'd be ok.
+#=======[test the loader]=============================
+ #   first_data = dataset.__getitem__(0)
+ #   (xVal, yLab) = first_data
+ #   print("features", xVal)
+ #   print("labels", yLab)
+
+                                                      #So this lossrate is good, DO NOT GO ANY Bigger
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=0.0001)
+
+
+#=============[THEHOOKS]====================================
+   # linearModule = model.get_submodule("lin2")  # DEFINITELY USING THIS
+   # handleL = linearModule.register_backward_hook(linearGradHook)  #CANT EVEN REGISTER FULL BACKWARD HOOK FOR A FIRST LAYER LINEAR MODULE
+  #  handleL1 = linearModule.register_forward_hook(forwardHook)
+    #pi_module = model.get_submodule("lPi")
+    #pi_handle = pi_module.register_full_backward_hook(piHook)
+
+
+# ===========[THEPARAMETERS_TO_TRACK]=========================
+   # print("THESE ARE THE PARAMETERS BY NAME", list(model.named_parameters()))
+  #  linWeight = model.get_parameter("lin.weight")
+    #ARE GRADIENTS PRINTABLE WITHOUT SAVING GRAD?
+  #  print(type(linWeight)) #<---- THIS IS NOT THE TENSOR BUT A PARAMETER. WRAPPED TENSOR.
+  #  print("DATA OF PARAMETER", linWeight.data)#<---- THE ACTUALL TENSOR
+  #  wHandle = linWeight.register_hook(lambda grad: print("THIS IS THE WEIGHT PARAMETER GRAD", grad)) #<---THIS IS RETURNING NO BATCH RELATED
+
+# ==================[Training]=======================================
+    terminatorCount = 0
+    oldLoss = 100
+    smallest = 5
+    terminate = False
+
+
+    epoch = 0
+    while(epoch<2000 and terminate==False):
+        batchIter = iter(dataloader)
+
+        i = 0
+        while(i < dataloader.__len__()):
+            try:
+                (features, targets) = batchIter.__next__()
+                features = features.to(device="cuda:0")
+                targets = targets.to(device="cuda:0")
+
+            except StopIteration:
+                break
+
+            #if (i%10 == 0):
+            #    handleL.remove()
+
+            #if (i%17 == 0) :
+            #    linearModule.register_backward_hook(linearGradHook)
+
+            (pi, mu, var) = model.forward(features)
+
+            if (epoch == 1000):
+                optimizer = torch.optim.AdamW(params=model.parameters(), lr=0.000005)
+            if(epoch > 1000):
+                totalLoss = linnearLoss_MLE(targets, pi, mu, var)
+            else:
+                totalLoss = logLoss_MLE(targets, pi, mu, var)
+
+
+            optimizer.zero_grad()
+            totalLoss.backward()
+
+            optimizer.step()
+
+
+            if(totalLoss.item() >= oldLoss):
+                terminatorCount += 1
+            else:
+                terminatorCount = 0
+            oldLoss = totalLoss.item()
+
+            if (terminatorCount > 50):
+                 terminate = True
+
+           # if (totalLoss.item() < -6.53351688):
+           #     terminate = True
+            if (totalLoss.item() < smallest):
+                smallest = totalLoss.item()
+
+            if epoch%1 == 0:
+                print("epoch:", epoch + 1, "total iterations. ", "loss: ", totalLoss.item(), "  TerminatorCount: ", terminatorCount)
+                # .item() GETS THE ACTUAL VALUE, MUST BE A 1,1 TENSOR
+
+
+        epoch = epoch + 1
+
+
+
+
+#========================[Save Model]=====================================================
+    print("Model's state parameters")
+    for p in model.state_dict():
+        print(p, "\t", model.state_dict()[p].size(), model.state_dict()[p].data)
+
+    torch.save(model.state_dict(), "./data/mixModel2.pt")
+
+
+
+if __name__ == '__main__':
+    main()
